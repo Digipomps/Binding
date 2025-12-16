cf8ad1cadb1c27222b584cdaa1a59043d189a90e
Kjetil Hustveit <kjetil.hustveit@bolder.no>
2025-12-16 14:26:59 +0100

    Initial Commit

---
+++ Binding/BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingApp.swift
+//  Binding
+//
+//  Created by Kjetil Hustveit on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
